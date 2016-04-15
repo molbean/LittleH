6de598b85ab170232dfad439e50e1389d9b4430d
g14941ok <g14941ok@s30517.tsuda.ac.jp>
2016-04-15 16:27:22 +0900

    ファースト、2回目のコミット。 セカンドさんよろしくお願いします。

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -81,7 +81,9 @@ final class StaticWordLibrary extends WordLibrary {
         "hotjava",
         "vertex",
         "unsigned",
-        "traditional"};
+        "traditional",
+        "computer",
+        "science"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
         "batsartcoin",
@@ -128,7 +130,9 @@ final class StaticWordLibrary extends WordLibrary {
         "ohjtvaa",
         "evtrxe",
         "nuisngde",
-        "rtdatioialn"
+        "rtdatioialn",
+        "mpuercot",
+        "cceeins"
     };
     
     final static WordLibrary DEFAULT = new StaticWordLibrary();
